feec26e191b0e25aee02e5e441bf4c18b392b8df
devwithkamati <devahi075@gmail.com>
2026-03-18 18:43:59 +0530

    Update something Password Controller

--- lib/controller/chnge_password_controller.dart
+++ lib/controller/chnge_password_controller.dart
@@ -7,41 +7,31 @@ import '../service/change_password_service.dart';
 import '../view/profileScreen/profile_page.dart';
 
 class ChangePasswordController extends GetxController {
+  final ChangePasswordService _service = ChangePasswordService();
 
-  final ChangePasswordService _service =
-  ChangePasswordService();
-
-  final FlutterSecureStorage _storage =
-  const FlutterSecureStorage();
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
   RxBool isLoading = false.obs;
 
   Future<void> changePassword(
-      String oldPassword,
-      String newPassword,
-      ) async {
-
+    String oldPassword,
+    String newPassword,
+  ) async {
     try {
-
       isLoading.value = true;
-
-      final id =
-      await _storage.read(key: "agentId");
-
+      final id = await _storage.read(key: "agentId");
       if (id == null) {
         Get.snackbar("Error", "User not logged in");
         return;
       }
 
-      final response =
-      await _service.changePassword(
+      final response = await _service.changePassword(
         agentId: int.parse(id),
         oldPassword: oldPassword,
         newPassword: newPassword,
       );
 
       if (response != null && response.status) {
-
         // ✅ Success Snackbar
         Get.snackbar(
           "Success",
@@ -56,24 +46,15 @@ class ChangePasswordController extends GetxController {
 
         // ✅ Navigate to Profile Screen
         Future.delayed(const Duration(milliseconds: 800), () {
-          Get.offAll(() =>  ProfilePage());
+          Get.offAll(() => ProfilePage());
         });
-
-      }else {
-
-        Get.snackbar(
-            "Error",
-            response?.message ??
-                "Change password failed");
+      } else {
+        Get.snackbar("Error", response?.message ?? "Change password failed");
       }
-
     } catch (e) {
-
       print("🔥 Change Password Error: $e");
 
-      Get.snackbar(
-          "Error",
-          "Something went wrong");
+      Get.snackbar("Error", "Something went wrong");
     } finally {
       isLoading.value = false;
     }
